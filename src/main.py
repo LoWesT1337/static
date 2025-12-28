6e3074b4542d09ca9dd951adeb6b6991dce7dbf2
lowest1337 <e1326220@student.tuwien.ac.at>
2025-12-28 16:03:57 +0100

    copy contents

--- src/main.py
+++ src/main.py
@@ -12,6 +12,10 @@ def copy_static(source_dir, dest_dir):
 
     # Iterate through source directory
     for item in os.listdir(source_dir):
+        # Skip the destination folder itself
+        if item == os.path.basename(dest_dir):
+            continue
+
         source_path = os.path.join(source_dir, item)
         dest_path = os.path.join(dest_dir, item)
 
@@ -19,12 +23,21 @@ def copy_static(source_dir, dest_dir):
             shutil.copy(source_path, dest_path)
             print(f"Copied file: {source_path} -> {dest_path}")
 
-        else:
+        elif os.path.isdir(source_path):
             print(f"Entering directory: {source_path}")
+            # Recursively copy subdirectory
+            os.mkdir(dest_path)
             copy_static(source_path, dest_path)
 
 def main():
-    copy_static("static", "public")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # The static directory is the parent of src
+    source_dir = os.path.abspath(os.path.join(current_dir, ".."))
+    dest_dir = os.path.join(source_dir, "public")
+    #print(source_dir)
+    #print(dest_dir)
+    copy_static(source_dir, dest_dir)
     #node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     #print(node)
 
